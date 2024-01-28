6057036472e2c9063b3b092463a2fba3944580a1
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-28 13:35:40 +0000

    chnage

---
+++ javascript/day5_datatype_and_ecma.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3+3) // show the error because we are using nodejs ,not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+   console.log("vaishu");
+   
+   let name = "vaishnavi"
+   let age = 23
+   let isLoggedId = false
+
+//number => 2 to power 53
+// bigint => if number is greater than 2 to power 53
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => value is not defined
+ // symbol =>  for uniqueness
+
+ //object
+
+ console.log(typeof undefined);// undefined
+ console.log(typeof null); //object 

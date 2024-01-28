697197776195dd2521c84535da6690b85213b066
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-28 13:55:51 +0000

    conversion of datatype

---
+++ javascript/day6_datatype_conversion.js
@@ -0,0 +1,31 @@
+let num = "66"
+
+// console.log(typeof num);
+// console.log(typeof(num));
+
+// let valueInNumber = Number(num)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+ /* "66"=>66
+  "66ab" => nan
+  true => 1
+  false => 0
+  null => 0
+  undefind => undefind
+ */
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+1 => true; 0 => false;
+"" => false
+"vaisshu" => true
+*/ 
+
+let someNum = 33
+let StringNum = String(someNum)
+console.log(StringNum)
+console.log( typeof StringNum)

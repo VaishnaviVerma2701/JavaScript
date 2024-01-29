ad691bf6aa7c3208b228697d9d8b65720a891b84
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-29 14:55:42 +0000

    datatype

--- javascript/1_dataType.js
+++ javascript/1_dataType.js
@@ -61,3 +61,42 @@ let someNum = 33
 let StringNum = String(someNum)
 console.log(StringNum)
 console.log( typeof StringNum)
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//  operation
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let  str2 = "vaishu"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2); // 12
+// console.log(2 + "1"); // 21
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+ 
+// if string is first than string operation perform first than Number
+// if number is first than number operation perform first than string
+
+console.log(true) //true
+console.log(+true) // 1
+console.log(false) // false
+console.log(+false) // 0
+console.log(""); // empty
+console.log(+""); //0
+
+
+let counter = 100
+counter++;
+console.log(counter);

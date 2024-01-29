729c37d0198823a4c73becf5d59719c0a8ffda39
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-29 21:07:40 +0000

    Object

--- javascript/day16object.js
+++ javascript/day16object.js
@@ -38,3 +38,57 @@ console.log(user.greeting());
 console.log(user.greeting1());
 console.log(user.greeting); //function anonymous
 
+
+
+//  DAY 17 OBJECT
+
+const tinderUser ={}
+tinderUser.id="123abd"
+tinderUser.name= "Sammy"
+tinderUser.isloggedIn = false
+//console.log(tinderUser)
+
+const regularUser ={
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+           firstname: "vaishnavi",
+           lastname: "verma"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+const obj1 = {1: "a" ,2: "b"}
+const obj2 = {3:"a", 4:"d"}
+const obj4 = {3:"a", 6:"d"}
+// const obj3 = {obj1,obj2}
+// console.log(obj3);
+
+// const obj3 = Object.assign({} ,obj1,obj2,obj4)
+// console.log(obj3);
+
+const obj3 = {...obj1, ...obj2,...obj4}
+ //console.log(obj3);
+
+ const Users = [
+{
+ id: 1,
+ name:"pihu"
+},
+{
+ id: 2,
+ name:"sanu"
+},
+{
+ id: 3,
+ name:"minu"
+}
+ ]
+ console.log(Users[1].name)
+
+ console.log(Object.keys(tinderUser));
+ console.log(Object.values(tinderUser));
+ console.log(Object.entries(tinderUser));
+ console.log((tinderUser.hasOwnProperty('id')));
+
+

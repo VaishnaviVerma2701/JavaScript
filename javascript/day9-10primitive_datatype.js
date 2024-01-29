8e98eace7ae0e703db59f101d15d9a0d43f75d43
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-29 15:52:51 +0000

    primitive datatype

--- javascript/day9-10primitive_datatype.js
+++ javascript/day9-10primitive_datatype.js
@@ -42,3 +42,29 @@ String => String
 Number => number
 Boolean =>boolean
 */
+
+
+// day10 stack and heap memory
+
+// Stack(Primitive) , Heap (Non-Primitive)
+
+// stact => store the copyvalue
+let myname = "vaishnaviverma"
+let anotherName = myname
+anotherName = "sandhya"
+
+console.log(anotherName);
+console.log(myname);
+
+ // Heap  => store the reference value
+let user ={
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+let userTwo = user
+userTwo.email = "userTwo@gmail.com" // access the object
+console.log(user.email);
+console.log(userTwo.email);
+
+
+

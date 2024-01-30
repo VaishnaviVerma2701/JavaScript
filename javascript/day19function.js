9119faaeb7a629a4aa2a3796db03a9765588c68d
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-30 10:20:32 +0000

    function

--- javascript/day19function.js
+++ javascript/day19function.js
@@ -12,10 +12,10 @@ myfunction() // write to execute the function
 function addTwonum(number1 ,number2) {  //function parameter
    console.log((number1+number2)); 
 }
-addTwonum(6,"a") //  this is function argument
+addTwonum(6,"a") //  this is function argument , print 6a
 
-const result = addTwonum(7,8) // return 15
- console.log("result :" ,result); //  return undefined
+const result = addTwonum(7,8) // print 15
+ console.log("result :" ,result); //  print undefined
 
 
  function addTwoNum(number1,number2){
@@ -24,7 +24,7 @@ const result = addTwonum(7,8) // return 15
     //return number1+ number2
  }
  const result1 = addTwoNum(3,6) // does not print any value because we initilize return kayword
- console.log("result :" , result1) // return 9
+ console.log("result :" , result1) // print 9
 
 
  function userlogin(username ="sam"){
@@ -40,4 +40,48 @@ const result = addTwonum(7,8) // return 15
  }
  //console.log(userlogin("vaishu"));
  console.log(userlogin());
- console.log(userlogin("nam")); // give the  then does not print sam
+ console.log(userlogin("nam")); // give the argument  then does not print sam
+ 
+
+
+ //    DAY 20 FUNCTION WITH OBJECT AND ARRAY
+
+ function calculateprice(num1) {
+   return num1
+ }
+ console.log(calculateprice(60)); // return 60
+ console.log(calculateprice(60,88,999)); // return 60
+
+
+ function calculateprice1(...num1) {
+   return num1
+ }
+ console.log("B=",calculateprice1(60,88,999))
+
+ function calculateprice2(val1, val2,...num1) {
+   return num1
+ }
+ console.log( "C=",calculateprice2(60,88,999,100,54,99)) // 
+
+ //using object
+ const user ={
+username: "mina",
+price : 123
+ }
+ function handleobj(anyobj) {
+   console.log(`username is ${anyobj.username} and price is ${anyobj.price}`);
+ }
+ handleobj(user) // print username mina and price 123
+
+ handleobj({ // print username sam and price 77
+   username :"sam",
+   price:77
+ })
+
+ //using array 
+
+ const myarr=[30,70,80,90]
+ function returnSecondValue(getarr) {
+   return getarr[1]  // print value of index 1
+ }
+ console.log("value of array index 1=",returnSecondValue(myarr));

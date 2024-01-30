4ce8fce42660665eb7c013b21a9f6df259a3d087
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-30 10:20:04 +0000

    object

--- javascript/day16-18object.js
+++ javascript/day16-18object.js
@@ -36,7 +36,7 @@ user.greeting1 = function(){
 }
 console.log(user.greeting());
 console.log(user.greeting1());
-console.log(user.greeting); //function anonymous
+console.log(user.greeting); // return function anonymous
 
 
 
@@ -58,6 +58,7 @@ const regularUser ={
     }
 }
 //console.log(regularUser.fullname.userfullname.firstname);
+
 const obj1 = {1: "a" ,2: "b"}
 const obj2 = {3:"a", 4:"d"}
 const obj4 = {3:"a", 6:"d"}

74c71362e28139682c98f309532ffe96540effb2
VaishnaviVerma2701 <150385881+VaishnaviVerma2701@users.noreply.github.com>
2024-01-31 12:50:49 +0000

    loop

--- javascript/27-28.loop.js
+++ javascript/27-28.loop.js
@@ -39,3 +39,29 @@
     }
     console.log(`value of elemenet id =${index}`); 
   }
+
+
+  // +++++  WHILE LOOP +++++ 
+
+  let index = 0
+  while(index <= 10){
+   //console.log(`value of index is ${index}`);
+   index = index + 2
+  }
+
+  let myarr = ["pen","copy","book","bag"]
+  let arr=0
+  while (arr < myarr.length) {
+   console.log(`value is ${myarr[arr]}`);
+   arr++
+  }
+
+  // +++ DO While Loop +++
+  let score = 1
+
+  do{
+   console.log(`scope is ${score}`);
+   score++
+  }
+  while(score <= 10);
+  
